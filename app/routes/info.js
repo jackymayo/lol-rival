efa90204adac542c62c8cbe5252c1188ba87989e
Jacky <jackymayonaise@gmail.com>
2018-05-16 20:39:24 -0400

    Fully functional color

--- app/routes/info.js
+++ app/routes/info.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // if (!fs.existsSync('/public/data/champion.json')){
 
 // }
-const api_key = 'RGAPI-cfc42fd1-d6af-4244-9920-99f826ee64f3'
+const api_key = 'RGAPI-d3ab0378-2e5d-495e-a4ec-a1797384849f'
 const api_key_query = '?api_key=' +  api_key;
 const patch_version = "8.9.1";
 const ddragon_host = 'http://ddragon.leagueoflegends.com/cdn/' + patch_version;
@@ -84,16 +84,24 @@ function retrieve_match(match_id){
     })
   });
 }
-function search_rival_in_matches(rival_id, match_body, ret){
-  var match_id = JSON.parse(match_body)['gameId'];
-  var participants = JSON.parse(match_body)['participantIdentities'];
+function search_rival_in_matches(user_id, rival_id, match_body, ret, misc_info){
+  var response = JSON.parse(match_body);
+  var match_id = response['gameId'];
+  var userWins = false;
+  console.warn(match_id);
+  var participants = response['participantIdentities'];
 
 	var left = [];
 	var right = [];
   for (var i = 0; i < participants.length; i++){
+    if (participants[i]['player']['currentAccountId'] == user_id){
+      misc_info.won = response['participants'][i]['stats']['win'];
+      misc_info.user_loc = i;
+    }
     if (participants[i]['player']['currentAccountId'] == rival_id){     
+      misc_info.rival_loc = i;
       for(var j = 0; j < 10; j++ ){  
-        var champID = JSON.parse(match_body)['participants'][j]['championId'];        
+        var champID = response['participants'][j]['championId'];        
         var champImage = image_url + getChampionById(championsJSON, champID);
         var summoner =  participants[j]['player']['summonerName'];
         if (Math.floor(j/ 5)  === 0){ left.push(champImage); left.push(summoner);}
@@ -103,8 +111,6 @@ function search_rival_in_matches(rival_id, match_body, ret){
 			ret.push(right);
     }
   }
-
-  console.warn("\n\n")
 
 }
 
@@ -121,8 +127,8 @@ router.get('/', function(req,res,next){
   var promise1 = get_account_id(username, options);
   var promise2 = get_account_id(rival_username, options);
   var promise3 = promise1.then(get_match_list, console.warn);
-  var list_of_promises = [promise2];
-  Promise.all([promise3, promise2]).then(function(values) {
+  var list_of_promises = [promise2, promise1];
+  Promise.all([promise3, promise2, promise1]).then(function(values) {
     for (var i = 0; i < 17; i++){ // Due to request restrictions
       list_of_promises.push(retrieve_match(values[0][i]['gameId']));
     }
@@ -130,18 +136,28 @@ router.get('/', function(req,res,next){
   })
   .then(function(values){
     const rival = values[0];  
-
+    const user = values[1];
+    // Turn this into an object array
     var all_matches = [];    
-    for(var i = 1; i < values.length; i++){
-			var ret = []
-      search_rival_in_matches(rival, values[i] , ret);
+    var match_results = [];
+    var user_locations = [];
+    var rival_locations = [];
+    for(var i = 2; i < values.length; i++){
+      var ret = []
+      var misc_info = { won: false, user_loc: 0, rival_loc: 0} ;
+      search_rival_in_matches(user, rival, values[i] , ret, misc_info, misc_info );
       if (ret.length !== 0){
+        match_results.push(misc_info.won);
         all_matches.push(ret);
       }
 		}
 		console.warn(all_matches);
-		console.warn(all_matches.length);
-    res.render('info', { username: username, rival: rival_username, match: all_matches, number: all_matches.length })    
+    console.warn(all_matches.length);
+    console.warn(match_results);
+    res.render('info', { username: username, rival: rival_username, 
+        match: all_matches, number: all_matches.length,
+      match_results: match_results, user_locations: user_locations,
+    rival_locations: rival_locations})    
   })
   .catch(console.warn);
   
