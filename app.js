7cd63598e3827415aef82a93e88661ba87e405d3
jackymayo <jackymayonaise@gmail.com>
2018-05-17 02:17:24 -0400

    add http

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var request = require('request');
 
+var http = require('http');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var infoRouter = require('./routes/info');
